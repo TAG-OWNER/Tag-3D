cfd894ec4d184a01adbb7b63a75d78ed32b05f86
TAG-OWNER <jjunte-sk@h3opo.nl>
2026-04-10 13:44:24 +0200

    Create initial React component for Tikkertje 3D

---
+++ src/Tikkertje3D.jsx
@@ -0,0 +1,12 @@
+// Tikkertje 3D React Component
+import React from 'react';
+
+const Tikkertje3D = () => {
+    return (
+        <div>
+            <h1>Tikkertje 3D Component</h1>
+        </div>
+    );
+};
+
+export default Tikkertje3D;
